2e8a3e64c4bcd7203cb8924319dbba2dc1518b46
NikitaS2001 <nikitasmadych2001@gmail.com>
2023-10-17 13:43:52 +1000

    add i2c connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // #include <stm32f1xx.h>
 #include <stm32f103x6.h>
+#include "i2c.h"
 
 #define DELAYSTAMP 600'000 // 10 minutes
 
@@ -7,11 +8,12 @@ volatile uint32_t timestamp = 600'000;
 
 void SysTick_Handler(void);
 
-
 int main(void)
 {
     if (SysTick_Config(SystemCoreClock / 1000)) { return 1; }
     
+    I2C1_Init(I2C_FAST);
+
     for(;;)
     {
         if (DELAYSTAMP < timestamp)
