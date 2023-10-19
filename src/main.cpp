8b8658038832b6596976308975d08e526fae7c69
NikitaS2001 <nikitasmadych2001@gmail.com>
2023-10-19 10:58:12 +1000

    minor update

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "i2c.h"
 #include "delay.h"
 #include "BMP280.h"
+#include "PCF8574.h"
 
 #define DELAYSTAMP 600'000 // 10 minutes
 
@@ -16,12 +17,16 @@ int main(void)
     DWT_Init();
     I2C1_Init(I2C_FAST);
     BMP280_Init(BMP280_Mode_Type::BMP280_Normal);
+    LCD_Init();
 
     for(;;)
     {
         if (DWT_nb_timeout(&delay))
         {
             BMP280_GetTemperatureAndPressure(&tem, &press);
+
+            
+
             DWT_nb_delay_ms(&delay, DELAYSTAMP);
         }
     }
