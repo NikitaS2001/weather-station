51351056782d3e8500bb8ddf2d1f671df1e76760
NikitaS2001 <nikitasmadych2001@gmail.com>
2023-10-19 10:56:16 +1000

    add pcf8574

---
+++ lib/pcf8574/src/PCF8574.cpp
@@ -0,0 +1,88 @@
+#include "PCF8574.h"
+
+void LCD_Init(void)
+{
+    DWT_delay_ms(50);
+    LCD_SendCmd(0x30);
+    DWT_delay_ms(5);
+    LCD_SendCmd(0x30);
+    DWT_delay_ms(50);
+    LCD_SendCmd(0x30);
+    DWT_delay_ms(10);
+    LCD_SendCmd(0x20); // 4bit mode
+    DWT_delay_ms(10);
+
+    LCD_SendCmd(0x28);
+    DWT_delay_ms(1);
+    LCD_SendCmd(0x08);
+    DWT_delay_ms(1);
+    LCD_SendCmd(0x01);
+    DWT_delay_ms(1);
+    DWT_delay_ms(1);
+    LCD_SendCmd(0x06);
+    DWT_delay_ms(1);
+    LCD_SendCmd(0x0C);
+}
+
+void LCD_SendCmd(char cmd)
+{
+    char data_u, data_l;
+    uint8_t data_t[4];
+    data_u = cmd & 0xf0;
+    data_l = (cmd << 4) & 0xf0;
+    data_t[0] = data_u | 0x0C;
+    data_t[1] = data_u | 0x08;
+    data_t[2] = data_l | 0x0C;
+    data_t[3] = data_l | 0x08;
+
+    // I2C Send
+	I2C1_Start();
+	I2C1_SendAddress(SLAVE_ADDRESS_LCD, I2C_TRANSMITTER);
+
+	for (int8_t i = 0; i < 4; ++i) {
+		I2C1_SendData(data_t[i]);
+	}
+
+	I2C1_Stop();
+    
+    DWT_delay_ms(100);
+
+}
+
+void LCD_SendData(char data)
+{
+    char data_u, data_l;
+	uint8_t data_t[4];
+	data_u = data & 0xf0;
+	data_l = (data << 4) & 0xf0;
+	data_t[0] = data_u | 0x0D;
+	data_t[1] = data_u | 0x09;
+	data_t[2] = data_l | 0x0D;
+	data_t[3] = data_l | 0x09;
+
+    // I2C Send
+	I2C1_Start();
+	I2C1_SendAddress(SLAVE_ADDRESS_LCD, I2C_TRANSMITTER);
+
+	for (int8_t i = 0; i < 4; ++i)
+    {
+		I2C1_SendData(data_t[i]);
+	}
+
+	I2C1_Stop();
+    
+    DWT_delay_ms(100);
+}
+void LCD_SendString(char* str)
+{
+    while (*str) { LCD_SendData(*str++); }
+}
+
+void LCD_Clear(void)
+{
+    LCD_SendCmd(0x80);
+    for (uint8_t i = 0; i < 70; i++)
+    {
+        LCD_SendData(' ');
+    }
+}
---
+++ lib/pcf8574/src/PCF8574.h
@@ -0,0 +1,16 @@
+#ifndef PCF8574_H_
+#define PCF8574_H_
+
+
+#include "i2c.h"
+#include "delay.h"
+
+#define SLAVE_ADDRESS_LCD 0x4E
+
+void LCD_Init(void);
+void LCD_SendCmd(char cmd);
+void LCD_SendData(char data);
+void LCD_SendString(char* str);
+void LCD_Clear(void);
+
+#endif /* PCF8574_H_ */
