49a97a8ec414724c9a513bd213f9e464e52ebd18
NikitaS2001 <nikitasmadych2001@gmail.com>
2023-10-23 21:57:33 +1000

    optimization code bme280

--- lib/bme280/bme280.c
+++ lib/bme280/bme280.c
@@ -60,55 +60,26 @@ int8_t BME280_Init (uint8_t osrs_t, uint8_t osrs_p, uint8_t osrs_h, uint8_t mode
 	ReadCalibrationData();
 
 	uint8_t datatowrite = 0;
-	uint8_t datacheck = 0;
 
 	// Reset the device
 	datatowrite = 0xB6;  // reset sequence
-	if (HAL_I2C_Mem_Write(BME280_I2C, BME280_ADDRESS_SHIFT, RESET_REG, 1, &datatowrite, 1, 1000) != HAL_OK)
-	{
-		return -1;
-	}
+	HAL_I2C_Mem_Write(BME280_I2C, BME280_ADDRESS_SHIFT, RESET_REG, 1, &datatowrite, 1, 1000);
 	HAL_Delay (100);
-
 
 	// write the humidity oversampling to 0xF2
 	datatowrite = osrs_h;
-	if (HAL_I2C_Mem_Write(BME280_I2C, BME280_ADDRESS_SHIFT, CTRL_HUM_REG, 1, &datatowrite, 1, 1000) != HAL_OK)
-	{
-		return -1;
-	}
+	HAL_I2C_Mem_Write(BME280_I2C, BME280_ADDRESS_SHIFT, CTRL_HUM_REG, 1, &datatowrite, 1, 1000);
 	HAL_Delay (100);
-	HAL_I2C_Mem_Read(BME280_I2C, BME280_ADDRESS_SHIFT, CTRL_HUM_REG, 1, &datacheck, 1, 1000);
-	if (datacheck != datatowrite)
-	{
-		return -1;
-	}
 
 	// write the standby time and IIR filter coeff to 0xF5
 	datatowrite = (t_sb << 5) | (filter << 2);
-	if (HAL_I2C_Mem_Write(BME280_I2C, BME280_ADDRESS_SHIFT, CONFIG_REG, 1, &datatowrite, 1, 1000) != HAL_OK)
-	{
-		return -1;
-	}
+	HAL_I2C_Mem_Write(BME280_I2C, BME280_ADDRESS_SHIFT, CONFIG_REG, 1, &datatowrite, 1, 1000);
 	HAL_Delay (100);
-	HAL_I2C_Mem_Read(BME280_I2C, BME280_ADDRESS_SHIFT, CONFIG_REG, 1, &datacheck, 1, 1000);
-	if (datacheck != datatowrite)
-	{
-		return -1;
-	}
 
 	// write the pressure and temp oversampling along with mode to 0xF4
 	datatowrite = (osrs_t << 5) | (osrs_p << 2) | mode;
-	if (HAL_I2C_Mem_Write(BME280_I2C, BME280_ADDRESS_SHIFT, CTRL_MEAS_REG, 1, &datatowrite, 1, 1000) != HAL_OK)
-	{
-		return -1;
-	}
+	HAL_I2C_Mem_Write(BME280_I2C, BME280_ADDRESS_SHIFT, CTRL_MEAS_REG, 1, &datatowrite, 1, 1000);
 	HAL_Delay (100);
-	HAL_I2C_Mem_Read(BME280_I2C, BME280_ADDRESS_SHIFT, CTRL_MEAS_REG, 1, &datacheck, 1, 1000);
-	if (datacheck != datatowrite)
-	{
-		return -1;
-	}
 
 	return 0;
 }
@@ -130,8 +101,8 @@ int BME280_Read_Raw()
 int32_t BME280_compensate_T(int32_t adc_T)
 {
 	int32_t var1, var2, T;
-	var1 = ((((adc_T >> 3) - ((int32_t)dig_T1 << 1))) * ((int32_t)dig_T2))  >>  11;
-	var2 = (((((adc_T >> 4) - ((int32_t)dig_T1)) * ((adc_T >> 4) - ((int32_t)dig_T1))) >> 12) *((int32_t)dig_T3))  >>  14;
+	var1 = ((((adc_T >> 3) - ((int32_t)dig_T1 << 1))) * ((int32_t)dig_T2)) >> 11;
+	var2 = (((((adc_T >> 4) - ((int32_t)dig_T1)) * ((adc_T >> 4) - ((int32_t)dig_T1))) >> 12) *((int32_t)dig_T3)) >> 14;
 	t_fine = var1 + var2;
 	T = (t_fine * 5 + 128) >> 8;
 	return T;
@@ -144,11 +115,11 @@ uint32_t BME280_compensate_P(int32_t adc_P)
 	var2 = var1 * var1 * (int64_t)dig_P6;
 	var2 = var2 + ((var1*(int64_t)dig_P5)<<17);
 	var2 = var2 + (((int64_t)dig_P4)<<35);
-	var1 = ((var1 * var1 * (int64_t)dig_P3) >> 8) + ((var1 * (int64_t)dig_P2)<<12);
-	var1 = (((((int64_t)1) << 47) + var1))*((int64_t)dig_P1) >> 33;
+	var1 = ((var1 * var1 * (int64_t)dig_P3) >> 8) + ((var1 * (int64_t)dig_P2) << 12);
+	var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)dig_P1) >> 33;
 	if (var1 == 0) {return 0;}
 	p = 1048576 - adc_P;
-	p = (((p << 31) - var2)*3125) / var1;
+	p = (((p << 31) - var2) * 3125) / var1;
 	var1 = (((int64_t)dig_P9) * (p >> 13) * (p >> 13)) >> 25;
 	var2 = (((int64_t)dig_P8) * p) >> 19;
 	p = ((p + var1 + var2) >> 8) + (((int64_t)dig_P7) << 4);
@@ -165,7 +136,7 @@ uint32_t bme280_compensate_H(int32_t adc_H)
 							((int32_t)32768))) >> 10) + ((int32_t)2097152)) * ((int32_t)dig_H2) +\
 					8192)  >>  14));
 	v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) *\
-			((int32_t)dig_H1))  >>  4));
+			((int32_t)dig_H1)) >> 4));
 	v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
 	v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
 	return (uint32_t)(v_x1_u32r >> 12);
